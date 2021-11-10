998755600ff363e69a53ef5e575322c403739423
PokenChupin <73985157+PokenChupin@users.noreply.github.com>
2021-11-10 20:35:03 +0800

    Add files via upload

--- CS3106_RSA.c
+++ CS3106_RSA.c
@@ -8,20 +8,17 @@ typedef struct{
 }Key;
 
 void initKeys(Key *, Key *);
-void encrypt(char msg[], Key pub);
-void decrypt(char msg[], Key pri);
-
-//Helper Functions
-double exponent(int base, int exp);
-
+void encrypt(unsigned char msg[], Key pub);
+void decrypt(unsigned char msg[], Key pri);
 
 int main(){
 	
 	int z;	
+	unsigned char msg[] = "RASTAMAN";
 	
 	Key pub, pri;
 	initKeys(&pub, &pri);
-	char msg[] = "RASTAMAN";
+	
 	printf("\n\nOriginal Message: %s",msg);
 	encrypt(msg,pub);
 	printf("\nEncrypted Message: %s",msg);
@@ -49,12 +46,12 @@ void initKeys(Key *pub, Key *pri){
 	pub->n = pri->n = n;
 	pri->de = d;
 	printf("\nRSA Settings:");
-	printf("\n p = %d\t; q = %d",p,q);
-	printf("\n e = %d\t; d = %d",pub->de,d);
-	printf("\n n = %d; z = %d",n,z);
+	printf("\n p = %3d; q = %d",p,q);
+	printf("\n e = %3d; d = %d",pub->de,d);
+	printf("\n n = %3d; z = %d",n,z);
 }
 
-void encrypt(char msg[], Key pub){
+void encrypt(unsigned char msg[], Key pub){
 	int i,size = strlen(msg);
 	int num,exp;
 	
@@ -62,13 +59,13 @@ void encrypt(char msg[], Key pub){
 		num = 1;
 		for(exp = 0; exp < pub.de;exp++){
 			num = num * msg[i];
-			num = num % pub.n;
-		}
+			num = num % pub.n;	
+		}	
 		msg[i] = num;
 	}
 }
 
-void decrypt(char msg[], Key pri){
+void decrypt(unsigned char msg[], Key pri){
 	int i,size = strlen(msg);
 	
 	int exp;
